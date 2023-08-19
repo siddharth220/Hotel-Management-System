0e18c20a4c66246df937b7210a0dbfd710c715d6
Siddharth Harshit <harshit.siddharth56@gmail.com>
2023-08-19 19:52:47 +0530

    Add Login page

--- src/HotelManagementSystem.java
+++ src/HotelManagementSystem.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class HotelManagementSystem extends JFrame {
+public class HotelManagementSystem extends JFrame implements ActionListener {
+
     HotelManagementSystem() {
         setBounds(100, 100, 1366, 565);
 
@@ -19,9 +21,14 @@ public class HotelManagementSystem extends JFrame {
         JButton nextPage = new JButton("Next Page");
         nextPage.setBounds(1220, 480, 100, 30);
         nextPage.setFont(new Font("Roboto", Font.BOLD, 12));
+        nextPage.addActionListener(this);
         homeImage.add(nextPage);
         setVisible(true);
         }
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
     public static void main(String[] args) {
         new HotelManagementSystem();
     }
--- src/Login.java
+++ src/Login.java
@@ -5,29 +5,48 @@ public class Login extends JFrame {
     Login() {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        setBounds(330, 120, 1000, 600);
-        setVisible(true);
+        setBounds(330, 120, 800, 300);
 
         JLabel usernameField = new JLabel("Username: ");
-        usernameField.setBounds(50, 20, 100, 100);
+        usernameField.setBounds(50, 50, 100, 30);
         usernameField.setFont(new Font("Roboto", Font.BOLD, 16));
         add(usernameField);
 
         JLabel passwordField = new JLabel("Password: ");
-        passwordField.setBounds(50, 70, 100, 100);
+        passwordField.setBounds(50, 100, 100, 30);
         passwordField.setFont(new Font("Roboto", Font.BOLD, 16));
         add(passwordField);
 
         JTextField usernameInput = new JTextField();
-        usernameInput.setBounds(100, 20, 300, 100);
-        usernameInput.setFont(new Font("Roboto", Font.BOLD, 16));
+        usernameInput.setBounds(150, 50, 300, 30);
         add(usernameInput);
-        
-        JTextField passwordInput = new JTextField();
-        passwordInput.setBounds(100, 70, 300, 100);
-        passwordInput.setFont(new Font("Roboto", Font.BOLD, 16));
+
+        JPasswordField passwordInput = new JPasswordField();
+        passwordInput.setBounds(150, 100, 300, 30);
         add(passwordInput);
 
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(80, 180, 150, 30);
+        loginButton.setBackground(Color.BLACK);
+        loginButton.setForeground(Color.white);
+        loginButton.setFont(new Font("Roboto", Font.BOLD, 18));
+        add(loginButton);
+
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(270, 180, 150, 30);
+        cancelButton.setBackground(Color.BLACK);
+        cancelButton.setForeground(Color.white);
+        cancelButton.setFont(new Font("Roboto", Font.BOLD, 18));
+        add(cancelButton);
+
+        ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+        Image imgCrop = img.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon imgCropConv = new ImageIcon(imgCrop);
+        JLabel loginImage = new JLabel(imgCropConv);
+        loginImage.setBounds(500, 30, 200, 200);
+        add(loginImage);
+
+        setVisible(true);
     }
     public static void main(String[] args) {
         new Login();
