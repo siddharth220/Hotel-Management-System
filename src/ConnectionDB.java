d1580ac63130f5e35d45bb3286aa2d29ba65d5c8
Siddharth Harshit <harshit.siddharth56@gmail.com>
2023-09-02 23:09:47 +0530

    Update ConnectionDB.java

--- src/ConnectionDB.java
+++ src/ConnectionDB.java
@@ -1,19 +1,28 @@
 import java.sql.*;
 
 public class ConnectionDB {
-    Connection c;
-    Statement s;
-    ConnectionDB() {
+    Connection connection;
+    Statement statement;
+
+    // Constructor to initialize the database connection and statement
+    public ConnectionDB() {
         try {
+            // Load the MySQL JDBC driver class
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3307/sunrisehotelmgmt", "root", "2022.Hell_cat*-");
-            s = c.createStatement();
+
+            // Establish a connection to the database
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/sunrisehotelmgmt", "root", "2022.Hell_cat*-");
+
+            // Create a statement for executing SQL queries
+            statement = connection.createStatement();
         } catch (Exception e) {
-            e.printStackTrace();  // Print the exception's stack trace for debugging purposes
+            // Print the exception's stack trace for debugging purposes
+            e.printStackTrace();
         }
-
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        // This is the main method, which is currently empty
+        // You can add code here for testing the database connection
     }
 }
