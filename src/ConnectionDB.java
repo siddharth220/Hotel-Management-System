d4c89e91edb5bb0ee48540ba067481756f61ded2
Siddharth Harshit <harshit.siddharth56@gmail.com>
2023-09-03 23:11:13 +0530

    Update ConnectionDB.java

--- src/ConnectionDB.java
+++ src/ConnectionDB.java
@@ -21,8 +21,26 @@ public class ConnectionDB {
         }
     }
 
+    // Method to validate the database connection
+    public boolean isConnectionValid() {
+        try {
+            // Check if the connection is not null and is still valid
+            return (connection != null) && (!connection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         // This is the main method, which is currently empty
         // You can add code here for testing the database connection
+        ConnectionDB dbConnection = new ConnectionDB();
+
+        if (dbConnection.isConnectionValid()) {
+            System.out.println("Database connection is valid.");
+        } else {
+            System.out.println("Database connection is not valid.");
+        }
     }
 }
